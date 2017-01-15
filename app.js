f64a1ba5af75d79e74c4fac54b3d55bb84a01c03
Guilherme Gomes <guilhermegomes09@gmail.com>
2017-01-15 17:04:57 -0300

    Create app.js

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
 (function () {
-	'use strict';
+'use strict';
 
-angular.module('LunchCheck', [])
+angular.module('ShoppingListCheckOff', [])
 
-.controller('LunchCheckController', LunchCheckController);
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-LunchCheckController.$inject = ['$scope'];
+// Implementação do controller 1
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController (ShoppingListCheckOffService) {
+  var tb = this;
 
-function LunchCheckController($scope) {
-  $scope.list = '';
-  $scope.message = '';
-  $scope.list2 = '';
-	
-  $scope.check = function(){
-    $scope.list2 = $scope.list.split(',')
-    
-    if (($scope.list2) == '') {
-      $scope.message = "Please enter data first"
-    } else if (($scope.list2.length) > 3){
-      $scope.message = "Too much!!"
-    } else  {
-      $scope.message = "Enjoy"
-    }
+  tb.markBought = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
   }
+
+  tb.buyList = ShoppingListCheckOffService.getToBuyList();
 }
 
-})();
+// Implementação do controller 2
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController (ShoppingListCheckOffService) {
+  var ab = this;
+
+  ab.alreadyBoughtList = ShoppingListCheckOffService.getAlreadyBoughtList();
+
+}
+
+function ShoppingListCheckOffService (){
+  var service = this;
+
+  service.item1 = {name: "cookie", quantity: 5};
+  service.item2 = {name: "apple", quantity: 8};
+  service.item3 = {name: "candy", quantity: 10};
+  service.item4 = {name: "fried chicken", quantity: 3};
+  service.item5 = {name: "something", quantity: 99};
+  service.toBuy  = [service.item1, service.item2, service.item3, service.item4, service.item5];
+
+  service.bought = [];
+
+  service.buyItem = function (itemIndex)  {
+    service.bought.push(service.toBuy[itemIndex]);
+    service.toBuy.splice(itemIndex,1);
+    // console.log(service.toBuy.length);
+  }
+
+  service.getToBuyList = function () {
+    return service.toBuy;
+  }
+
+  service.getAlreadyBoughtList = function () {
+    return service.bought;
+  }
+
+}
+
+}());
